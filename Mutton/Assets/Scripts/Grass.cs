021429eb300aa76229eb158afa24694c46a85ba6
Greg Chudecke <greg.chudecke@gmail.com>
2015-01-24 11:38:42 -0800

    Tile Manager Working for collecting grass

--- Mutton/Assets/Scripts/Grass.cs
+++ Mutton/Assets/Scripts/Grass.cs
@@ -3,17 +3,22 @@
 /// <summary>
 /// Grass class.
 /// </summary>
-public class Grass : MonoBehaviour
+public class Grass : TileGround
 {
 	internal void OnTriggerEnter(Collider other) 
 	{
+		Debug.Log ("TRIGGER ENTER");
 		var player = other.GetComponent<Player>();
 		if (player != null)
 		{
 			player.Trample();
 		}
 
+		//Set
+		SetState(ETileState.dirt);
+
+
 		// TODO: replace this with a trampled grass prefab
-		Destroy(this.gameObject);
+		//Destroy(this.gameObject);
 	}
 }
--- Mutton/Assets/Scripts/TileGround.cs
+++ Mutton/Assets/Scripts/TileGround.cs
@@ -1,19 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum ETileState
+{
+	dirt,
+	grass
+}
+
 public class TileGround : MonoBehaviour 
 {
 
 	public GameObject _dirt;
 	public GameObject _grass;
+	public ETileState _state = ETileState.grass;
 
 	// Use this for initialization
 	void Start () {
-	
+		SetState (ETileState.grass);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void SetState(ETileState state)
+	{
+		Debug.Log (state.ToString ());
+		if (state == _state) 
+		{
+			return;
+		}
+
+		switch(state)
+		{
+			case ETileState.dirt:
+			{
+				_dirt.SetActive(true);
+				_grass.SetActive(false);
+				break;
+			}
+			case ETileState.grass:
+			{
+				_dirt.SetActive(false);
+				_grass.SetActive(true);
+				break;
+			}
+		}
+
+		_state = state;
 	}
 }
