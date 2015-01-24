b572638eba2677ec3357f7836d4d05a31541a66a
Shawn <feddas00@gmail.com>
2015-01-24 14:36:08 -0800

    Player 1 objective selection

---
+++ Mutton/Assets/Scripts/Globals.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Globals
+{
+    public Dictionary<PlayerId, ScoreType> Objective = new Dictionary<PlayerId, ScoreType>();
+
+    public static Globals Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new Globals();
+            return instance;
+        }
+        set { instance = value; }
+    }
+    private static Globals instance;
+}
---
+++ Mutton/Assets/Scripts/SelectObjective.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SelectObjective : MonoBehaviour
+{
+    System.Collections.Generic.Dictionary<DirectionEnum, ScoreType> Objectives = new Dictionary<DirectionEnum, ScoreType>()
+    {
+        {DirectionEnum.Up, ScoreType.Eat},
+        {DirectionEnum.Down, ScoreType.Plant},
+        {DirectionEnum.Right, ScoreType.Capture},
+        {DirectionEnum.Left, ScoreType.Trample},
+    };
+
+    private bool startCounter;
+
+    void Start()
+    {
+        Debug.Log("SecretObjective");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startCounter) ; // start counter
+
+        SetObjective(PlayerId.One);
+    }
+
+    private void SetObjective(PlayerId player)
+    {
+        float xAxis = Input.GetAxisRaw("HorizontalGP" + (int)player);
+        float yAxis = Input.GetAxisRaw("VerticalGP" + (int)player);
+        if (xAxis != 0 && yAxis != 0)
+        {
+            startCounter = true;
+            DirectionEnum dir = getDirection(xAxis, yAxis);
+
+            Globals.Instance.Objective[player] = Objectives[dir];
+            Debug.Log(xAxis + ", " + yAxis
+                + dir + Globals.Instance.Objective[player]);
+        }
+    }
+
+    private DirectionEnum getDirection(float horizontal, float vertical)
+    {
+        if (Mathf.Abs(horizontal) > Mathf.Abs(vertical))
+        {
+            if (horizontal > 0)
+                return DirectionEnum.Right;
+            else
+                return DirectionEnum.Left;
+        }
+        else if (vertical != 0)
+        {
+            if (vertical > 0)
+                return DirectionEnum.Up;
+            else
+                return DirectionEnum.Down;
+        }
+        else
+            return DirectionEnum.None;
+    }
+
+    private void CounterFinished()
+    {
+        Application.LoadLevel("Farm");
+    }
+}
