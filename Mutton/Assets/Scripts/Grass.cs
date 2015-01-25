e85981c413602f33893f2a6bf5059a7214918238
Greg Chudecke <greg.chudecke@gmail.com>
2015-01-24 20:13:59 -0800

    Checking in Ground And Farm updates

--- Mutton/Assets/Scripts/Grass.cs
+++ Mutton/Assets/Scripts/Grass.cs
@@ -7,6 +7,7 @@ public class Grass : TileGround
 {
 	internal void OnTriggerEnter(Collider other) 
 	{
+		//Debug.Log ("TRIGGER ENTER");
 		Player player = other.GetComponent<Player>();
 		if (player != null && player.PlayerState == PlayerStates.Trample)
 		{
