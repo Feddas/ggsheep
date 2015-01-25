38a36cd958a038119b9250c73368887279f092e0
Greg Chudecke <greg.chudecke@gmail.com>
2015-01-25 01:25:38 -0800

    1) Shadow on 2) Tile manager random spawn

--- Mutton/Assets/Scripts/TileManager.cs
+++ Mutton/Assets/Scripts/TileManager.cs
@@ -1,7 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TileManager : MonoBehaviour {
+public class TileManager : MonoBehaviour 
+{
+	public GameObject _sheep;
+	public GameObject _pen;
+
+	[System.Serializable]
+	public struct BoundPair
+	{
+		public BoundPair(int min,int max)
+		{
+			_min = min;
+			_max = max;
+		}
+
+		public int GetRandom()
+		{
+			return Random.Range(_min,_max);
+		}
+
+		public int _min;
+		public int _max;
+	};
 
 	static public TileManager instance;
 
@@ -9,6 +30,12 @@ public class TileManager : MonoBehaviour {
 	public bool _continuousGeneration;
 	private float m_timeBetweenGeneration = 3.0f;
 	public Vector3 m_playerOffset = new Vector3(0.0f,10.0f,0.0f);
+
+
+	System.Collections.Generic.KeyValuePair<int, int> rangeSheep; 
+	public BoundPair boundSheep = new BoundPair(6,24);
+	public BoundPair boundPen = new BoundPair(1,4);
+
 
 	public float _respawnHeight = -5.0f;
 
@@ -49,19 +76,36 @@ public class TileManager : MonoBehaviour {
 	{
 		instance = this;
 
-		if( _levelGenerator != null )
+
+		SpawnTiles();
+		SpawnSheep();
+		SpawnPens();
+
+	}
+
+	void SpawnSheep ()
+	{
+		int numberSheep = boundSheep.GetRandom(); 
+		for(int i=0;i<numberSheep;++i)
 		{
-			PopulateMapRaycast();
-		}
-		else
-		{
-			PopulateMap();
+			Vector3 pos = GetSpawnPosition();
+			Sheep sheep = (Instantiate(_sheep,pos,Quaternion.identity) as GameObject).GetComponent<Sheep>();
+			sheep.transform.parent = gameObject.transform;
 		}
 	}
 
-
+	void SpawnPens ()
+	{
+		int numberPen = boundPen.GetRandom(); 
+		for(int i=0;i<numberPen;++i)
+		{
+			Vector3 pos = GetSpawnPosition();
+			Pen pen = (Instantiate(_pen,pos,Quaternion.identity) as GameObject).GetComponent<Pen>();
+			pen.transform.parent = gameObject.transform;
+		}
+	}
 	
-	// Update is called once per frame
+	// Update is SpawnTilesame
 	void Update () 
 	{
 		RunningGeneration = _continuousGeneration;
@@ -101,7 +145,7 @@ public class TileManager : MonoBehaviour {
 		return Vector3.zero;
 	}
 
-	void PopulateMapRaycast()
+	void SpawnTiles()
 	{
 		//Destory any previous map
 		ClearMap ();
@@ -127,23 +171,6 @@ public class TileManager : MonoBehaviour {
 			}
 		}
 
-	}
-
-	void PopulateMap()
-	{
-		//Calculate the start tile position
-		Vector3 vStart = new Vector3 (-(float)_width * 0.5f, 0.0f, -(float)_height * 0.5f);
-
-
-		m_tiles = new TileGround[_height,_width];
-		for (int j=0; j<_height; ++j) 
-		{
-			for (int i=0; i<_width; ++i) 
-			{
-				m_tiles[j,i] = (TileGround)Instantiate(_tileGround,vStart + new Vector3((float)i,0.0f,(float)j),Quaternion.identity);
-				m_tiles[j,i].transform.parent = gameObject.transform;
-			}
-		}
 	}
 
 	public TileGround _tileGround; 
